0f2e8b3dc92d60e315748ea03139193e27c25ef2
kashishtkarkera <kashishkarkera0@gmail.com>
2025-08-31 13:37:40 +0530

    Added Campus Courier Lab program

---
+++ Main.java
@@ -0,0 +1,19 @@
+package main;
+import transport.*;
+
+public class Main {
+	
+
+	public static void main(String[] args) {
+		
+		EBike e = new EBike( "EB-101", 50 );
+		e.deliver("Sandwich", "Library");
+		Drone d = new Drone( "DR-1");
+		d.deliver("Notes", "ExamCell");
+		d.deliver("USB",  "ISE Block");
+		double bill = d.cost(5);
+		System.out.println("Drone delivery cost: Rs." +  bill);
+	}
+
+}
+
