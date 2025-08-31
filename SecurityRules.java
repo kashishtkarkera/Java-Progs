f993eab2e2062b78ad3a17985e07dc4d46e2a3c6
kashishtkarkera <kashishkarkera0@gmail.com>
2025-08-31 13:35:19 +0530

    Added Campus Courier Lab program

---
+++ SecurityRules.java
@@ -0,0 +1,18 @@
+package transport;
+
+public final class SecurityRules {
+
+	private SecurityRules() {}
+	
+		public static boolean canFly (String place) {
+		if (place == "ExamCell") {
+			return false;	
+	    }
+		else {
+			return true;
+		}
+		
+	}
+
+}
+
