b1142f82cfa13a11f80eb3aeca34766149425479
kashishtkarkera <kashishkarkera0@gmail.com>
2025-10-06 21:08:00 +0530

    Added College Notice Board Lab program

---
+++ ProducerConsumerDemo.java
@@ -0,0 +1,23 @@
+package noticeboard;
+
+public class ProducerConsumerDemo {
+	public static void main(String[] args) {
+		 MessageBoard board = new MessageBoard();
+	        Producer p = new Producer(board);
+	        Consumer c = new Consumer(board);
+
+	        p.start();
+	        c.start();
+
+	        try {
+	            p.join();
+	            c.join();
+	        } catch (InterruptedException e) {
+	            Thread.currentThread().interrupt();
+	        }
+	    }
+	}
+		
+	
+
+
