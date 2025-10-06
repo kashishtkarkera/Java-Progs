1f7857c1b61253a9614bff5cb5396c162e3cf74d
kashishtkarkera <kashishkarkera0@gmail.com>
2025-10-06 21:07:10 +0530

    Added College Notice Board Lab program

---
+++ Producer.java
@@ -0,0 +1,32 @@
+package noticeboard;
+
+public class Producer extends Thread {
+	 private MessageBoard board;
+
+	    public Producer(MessageBoard b) {
+	        this.board = b;
+	    }
+
+	   
+	    public void run() {
+	        String[] msgs = {
+	            "Exam on Monday",
+	            "Holiday on Tuesday",
+	            "Workshop on Wednesday"
+	        };
+
+	        for (String msg : msgs) {
+	            board.put(msg);
+	            try {
+	                Thread.sleep(1000); 
+	            } catch (InterruptedException e) {
+	                Thread.currentThread().interrupt();
+	            }
+	        }
+	    }
+	}
+
+
+	
+
+
