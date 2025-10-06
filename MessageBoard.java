f5b38922e2458cec8e05b647ea7a5c3dfa742ba7
kashishtkarkera <kashishkarkera0@gmail.com>
2025-10-06 21:06:35 +0530

    Added College Notice Board Lab program

---
+++ MessageBoard.java
@@ -0,0 +1,38 @@
+package noticeboard;
+
+public class MessageBoard {
+	 private String message;
+	    private boolean hasMessage = false;
+
+	    public synchronized void put(String msg) {
+	        while (hasMessage) {
+	            try {
+	                wait(); 
+	            } catch (InterruptedException e) {
+	                Thread.currentThread().interrupt();
+	            }
+	        }
+	        message = msg;
+	        hasMessage = true;
+	        System.out.println("Producer sends: " + msg);
+	        notify(); 
+	    }
+
+	    public synchronized String get() {
+	        while (!hasMessage) {
+	            try {
+	                wait();
+	            } catch (InterruptedException e) {
+	                Thread.currentThread().interrupt();
+	            }
+	        }
+	        String msg = message;
+	        hasMessage = false;
+	        System.out.println("Consumer reads: " + msg);
+	        notify(); 
+	        return msg;
+	    }
+	}
+
+	
+
